9f96a2304b0adb65fec0d4a6f6206191cac97ec3
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-01-17 14:25:21 +0530

    Update compress.rs

--- src/compress.rs
+++ src/compress.rs
@@ -1,6 +1,19 @@
-use std::io::Result;
-
-pub fn compress(input: &str, output: &str) -> Result<()> {
-    println!("Compressing {} → {}", input, output);
+use std::fs::File;
+use std::io::{Write, Result};
+use zip::write::FileOptions;
+use zip::{ZipWriter,CompressionMethod};
+use crate::file_io;
+/// Compress one file into a ZIP archive using DEFLATE
+pub fn compress_single_file(input:&str,output:&str)->Result<()> {
+    let d=file_io::rf(input)?;
+    let original_size = d.len();
+    let zip_file = File::create(output)?;
+    let mut zip = ZipWriter::new(zip_file);
+    let options=FileOptions::default().compression_method(CompressionMethod::Deflated).compression_level(Some(9)); //compression
+    zip.start_file(input, options)?;
+    zip.write_all(&d)?;
+    zip.finish()?;
+    println!("Original size:{} bytes", original_size);
+    println!("ZIP created:{}",output);
     Ok(())
 }
