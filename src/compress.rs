b92863976a4451c700371972e879948083bb8cf0
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-01-31 13:50:25 +0530

    Update compress.rs -

--- src/compress.rs
+++ src/compress.rs
@@ -1,19 +1,18 @@
 use std::fs::File;
-use std::io::{Write, Result};
+use std::io::{Write,Result};
+use zip::ZipWriter;
 use zip::write::FileOptions;
-use zip::{ZipWriter,CompressionMethod};
 use crate::file_io;
-/// Compress one file into a ZIP archive using DEFLATE
-pub fn compress_single_file(input:&str,output:&str)->Result<()> {
-    let d=file_io::rf(input)?;
-    let original_size = d.len();
-    let zip_file = File::create(output)?;
-    let mut zip = ZipWriter::new(zip_file);
-    let options=FileOptions::default().compression_method(CompressionMethod::Deflated).compression_level(Some(9)); //compression
-    zip.start_file(input, options)?;
-    zip.write_all(&d)?;
+pub fn single(input:&str,output:&str)->Result<()>{
+    let data=file_io::to_read(input)?;
+    let org_size=data.len();
+    let zipf=File::create(output)?;
+    let mut zip=ZipWriter::new(zipf);
+    let options=FileOptions::default();
+    zip.start_file(input,options)?;
+    zip.write_all(&data)?;
     zip.finish()?;
-    println!("Original size:{} bytes", original_size);
-    println!("ZIP created:{}",output);
+    println!("{}",org_size);
+    println!("{}",output);
     Ok(())
 }
