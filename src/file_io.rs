49586b0e1b47dc047f24ab0ed88145341c492aaa
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-01-31 12:41:57 +0530

    Update file_io.rs - added file handling

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,14 +1,16 @@
 use std::fs::File;
-use std::io::{Read, Write, Result};
-pub fn rf(path:&str)->Result<Vec<u8>> {
+use std::io::{Read,Write,Result};
+pub fn to_read(path:&str)->Result<Vec<u8>>{
     let mut f=File::open(path)?;
     let mut buf=Vec::new();
     f.read_to_end(&mut buf)?;
     Ok(buf)
 }
-pub fn wf(path:&str,d:&[u8])->Result<()> {
+pub fn to_write(path:&str,data:&[u8])->Result<()>{
     let mut f=File::create(path)?;
-    f.write_all(d)?;
+    f.write_all(data)?;
     Ok(())
 }
-
+pub fn size(path:&str)->Result<u64>{
+    Ok(std::fs::metadata(path)?.len())
+}
