4ad16ea0b3ed28f8823da63ebb30c2b4dd80b894
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-01-31 13:51:19 +0530

    Update compress.rs

--- src/compress.rs
+++ src/compress.rs
@@ -5,14 +5,14 @@ use zip::write::FileOptions;
 use crate::file_io;
 pub fn single(input:&str,output:&str)->Result<()>{
     let data=file_io::to_read(input)?;
-    let org_size=data.len();
+    let org_size=data.len(); //optional to get understand how file size is reduced
     let zipf=File::create(output)?;
     let mut zip=ZipWriter::new(zipf);
     let options=FileOptions::default();
     zip.start_file(input,options)?;
     zip.write_all(&data)?;
     zip.finish()?;
-    println!("{}",org_size);
+    println!("{}",org_size); //optional
     println!("{}",output);
     Ok(())
 }
