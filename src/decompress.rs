fa059431202868f0a3c16e819cc54b0d217ef4db
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-02-03 17:34:47 +0530

    Update decompress.rs

--- src/decompress.rs
+++ src/decompress.rs
@@ -1,6 +1,24 @@
-use std::io::Result;
+use std::fs::{File,create_dir_all};
+use std::io::{Result,copy};
+use std::path::Path;
+use zip::ZipArchive;
 
-pub fn decompress(input: &str, output: &str) -> Result<()> {
-    println!("Decompressing {} → {}", input, output);
+pub fn decomp(zip_path:&str,output_dir:&str)->Result<()>{
+    let file=File::open(zip_path)?;
+    let mut a=ZipArchive::new(file)?;
+    for i in 0..a.len(){
+        let mut e=a.by_index(i)?;
+        let outpath=Path::new(output_dir).join(e.name());
+        if e.is_dir(){
+            create_dir_all(&outpath)?;
+        }else{
+            if let Some(parent)=outpath.parent(){
+                create_dir_all(parent)?;
+            }
+            let mut out=File::create(&outpath)?;
+            copy(&mut e,&mut out)?;
+        }
+    }
+    println!("Decompressed{}",output_dir);
     Ok(())
 }
