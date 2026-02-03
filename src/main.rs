d4b12eb83830cfa57eedd1fd65e8bad0c8834051
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-02-03 17:33:58 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod file_io;
+mod utils;
+mod compress;
+mod decompress;
 use clap::{Parser, Subcommand};
 use std::io::Result;
+
 #[derive(Parser)]
 #[command(name="hi")]
 #[command(about="It`s just fun name")]
@@ -14,7 +18,27 @@ enum Command{
         input:String,
         output:String,
     },
+    Compress{
+        input:Vec<String>,
+        #[arg(short, long)]
+        output:String
+    },
+    Folder{
+        input:String,
+        #[arg(short,long)]
+        output:String
+    },
+    Decompressed{
+        zip:String,
+        #[arg(short,long)]
+        output:String,
+    },
+    CompressDir{
+        dir:String,
+        output:String,
+    },
 }
+
 fn main()->Result<()>{
     let cli=Cli::parse();
     match cli.command{
@@ -26,6 +50,23 @@ fn main()->Result<()>{
             let data=file_io::to_read(&input)?;
             file_io::to_write(&output,&data)?;
             println!("File copied");
+        }
+        Command::Compress{input , output}=>{
+            if input.len()==1{
+                compress::single(&input[0],&output)?;
+            }
+            else{
+                compress::multi(input,&output)?;
+            }
+        }
+        Command::Folder {input,output}=>{
+        compress::folder(&input,&output)?;
+    }
+        Command::Decompressed {zip, output }=>{
+            decompress::decomp(&zip,&output)?;
+        }
+        Command::CompressDir { dir, output }=>{
+            compress::folder(&dir,&output)?;
         }
     }
     Ok(())
