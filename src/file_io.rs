b05e00cc808d1e89acf72d78bcfe2902f27f966e
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-02-11 19:44:37 +0530

    Update file_io.rs

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,16 +1,19 @@
 use std::fs::File;
 use std::io::{Read,Write,Result};
+
 pub fn to_read(path:&str)->Result<Vec<u8>>{
     let mut f=File::open(path)?;
     let mut buf=Vec::new();
     f.read_to_end(&mut buf)?;
     Ok(buf)
 }
+
 pub fn to_write(path:&str,data:&[u8])->Result<()>{
     let mut f=File::create(path)?;
     f.write_all(data)?;
     Ok(())
 }
+
 pub fn size(path:&str)->Result<u64>{
     Ok(std::fs::metadata(path)?.len())
 }
