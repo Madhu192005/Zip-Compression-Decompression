4286a5cd2858e08d002c1f8ac46940d7080551a3
Madhusudhanan S <147263453+Madhu192005@users.noreply.github.com>
2026-02-24 21:34:55 +0530

    Update manual.rs

--- src/manual.rs
+++ src/manual.rs
@@ -57,3 +57,69 @@ fn main()->std::io::Result<()>{
     println!("zip manually done!");
     Ok(())
 }
+
+Without helper
+use std::fs::File;
+use std::io::{Write,Seek,SeekFrom,Result};
+use crc32fast::hash;
+
+fn write_u16_le<W: Write>(w: &mut W, v: u16) -> Result<()> {
+    w.write_all(&v.to_le_bytes())
+}
+
+fn write_u32_le<W: Write>(w: &mut W, v: u32) -> Result<()> {
+    w.write_all(&v.to_le_bytes())
+}
+fn main()->std::io::Result<()>{
+    let mut zip=File::create("my first manual zip")?;
+    let filename=b"example.txt ";
+    let data=b"Hello ZIP";
+    let crc32=hash(data);
+    let size=data.len() as u32;
+
+    //Local file header
+    let localh=zip.seek(SeekFrom::Current(0))? as u32;
+    zip.write_all(&[0x50,0x4B,0x03,0x04])?;
+    write_u16_le(&mut zip,20)?;
+    write_u16_le(&mut zip,0)?;  // flags
+    write_u16_le(&mut zip,0)?;  // compression (0 = store)
+    write_u16_le(&mut zip,0)?;  // mod time
+    write_u16_le(&mut zip,0)?; //mod date
+    write_u32_le(&mut zip,crc32)?;
+    write_u32_le(&mut zip,size)?;
+    write_u32_le(&mut zip,size)?;
+    write_u16_le(&mut zip,filename.len() as u16)?;
+    write_u16_le(&mut zip,0)?; // extra length
+    zip.write_all(data)?;
+    //central dir
+    let cdo =
+    zip.seek(SeekFrom::Current(0))? as u32;
+    zip.write_all(&[0x50, 0x4B, 0x01, 0x02])?;
+    write_u16_le(&mut zip,20)?;
+    write_u16_le(&mut zip,0)?;  // flags
+    write_u16_le(&mut zip,0)?;  // compression (0 = store)
+    write_u16_le(&mut zip,0)?;  // mod time
+    write_u16_le(&mut zip,0)?; //mod date
+    write_u32_le(&mut zip,crc32)?;
+    write_u32_le(&mut zip,size)?;
+    write_u32_le(&mut zip,size)?;
+    write_u16_le(&mut zip,filename.len() as u16)?;
+    write_u16_le(&mut zip,0)?; // extra length
+    write_u16_le(&mut zip,0)?;
+    write_u16_le(&mut zip,0)?;
+    write_u16_le(&mut zip,0)?;
+    write_u32_le(&mut zip,0)?;
+    write_u32_le(&mut zip,localh)?;
+    zip.write_all(filename)?;
+    let end=zip.seek(SeekFrom::Current(0))? as u32;
+    let cds=end-cdo;
+    write_u16_le(&mut zip,0)?;
+    write_u16_le(&mut zip,0)?;
+    write_u16_le(&mut zip,1)?;
+    write_u16_le(&mut zip,1)?;
+    write_u32_le(&mut zip,cds)?;
+    write_u32_le(&mut zip,cdo)?;
+    write_u16_le(&mut zip,0)?; //comment
+    println!("Manual");
+    Ok(())
+}
